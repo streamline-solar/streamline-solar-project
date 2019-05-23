bd5179b6a8d61567641cceb4fe3666c643f5fbd1
jesse-dahl <jddahl199@gmail.com>
2019-05-23 00:05:45 -0600

    just a shell of a React app. going to focus on this project for a while

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./../styles/index.scss";
+//import { Link } from 'react-router-dom';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="main-body">
+      </div>
+    );
+  }
+}
+
+export default Home;
