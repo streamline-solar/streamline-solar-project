2783ffd2c73dfc3b9e62d93d3bf7abca6ee7bc24
jesse-dahl <jddahl199@gmail.com>
2019-05-25 17:47:44 -0600

    added stuff to main page. almost finished with the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import "./../styles/index.scss";
 import Logo from "./../images/streamline-solar-logo.png";
+import ProjectImage from "./../images/home-project-image.jpeg";
+import ProgressImage from "./../images/home-progress-image.jpeg";
+import PartnerImage from "./../images/home-partners-image.jpg";
+import ContactImage from "./../images/home-contact-us-image.jpeg";
+import PhotosImage from "./../images/home-images-image.jpg";
 //import { Link } from 'react-router-dom';
 
 class Home extends Component {
@@ -71,6 +76,106 @@ class Home extends Component {
                   <div className="seperator-block">
                     <hr className="seperator"></hr>
                   </div>
+                <div className="home-link-content-wrapper">
+                  <div className="home-link-content">
+
+                    <div className="single-image-wrapper AlignLeft JustSomePadding">
+                      <div className="image-content-wrapper FlexCenter">
+                        <img className="QuickLinkImage" id="project-link-image" src={ProjectImage} alt="just a random image"></img>
+                        <div className="image-text-block">
+                          <div className="image-text-title">
+                            <h2 className="ImageTitle">The Project</h2>
+                          </div>
+                          <div className="image-text-description">
+                            <p className="ImageBody">Wondering what we're all about? Find out more about our story, goals, and student involvement.</p>
+                          </div>
+                          <div className="button-wrapper FlexRowCenter">
+                            <div className="button-content">
+                              <a href="/" className="Button ImageButton FlexCenter">Learn More</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                  <div className="single-image-wrapper AlignRight JustSomePadding">
+                    <div className="image-content-wrapper FlexCenter">
+                      <img className="QuickLinkImage" id="project-link-image" src={ProgressImage} alt="just a random image"></img>
+                      <div className="image-text-block">
+                        <div className="image-text-title">
+                          <h2 className="ImageTitle">Our Progress</h2>
+                        </div>
+                        <div className="image-text-description">
+                          <p className="ImageBody">Curious about where we are in the process and what we are working on now? Stay up to date!</p>
+                        </div>
+                        <div className="button-wrapper FlexRowCenter">
+                          <div className="button-content">
+                            <a href="/" className="Button ImageButton FlexCenter">Explore</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="single-image-wrapper AlignLeft JustSomePadding">
+                    <div className="image-content-wrapper FlexCenter">
+                      <img className="QuickLinkImage" id="project-link-image" src={PartnerImage} alt="just a random image"></img>
+                      <div className="image-text-block">
+                        <div className="image-text-title">
+                          <h2 className="ImageTitle">Our Partners</h2>
+                        </div>
+                        <div className="image-text-description">
+                          <p className="ImageBody">Excited about the project? See who has contributed to our efforts and consider joining them!</p>
+                        </div>
+                        <div className="button-wrapper FlexRowCenter">
+                          <div className="button-content">
+                            <a href="/" className="Button ImageButton FlexCenter">Find Out How</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="single-image-wrapper AlignRight JustSomePadding">
+                    <div className="image-content-wrapper FlexCenter">
+                      <img className="QuickLinkImage" id="project-link-image" src={PhotosImage} alt="just a random image"></img>
+                      <div className="image-text-block">
+                        <div className="image-text-title">
+                          <h2 className="ImageTitle">Photos</h2>
+                        </div>
+                        <div className="image-text-description">
+                          <p className="ImageBody">Want to see our team in action? Check out our pictures!</p>
+                        </div>
+                        <div className="button-wrapper FlexRowCenter">
+                          <div className="button-content">
+                            <a href="/" className="Button ImageButton FlexCenter">See More</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="single-image-wrapper AlignLeft JustSomePadding">
+                    <div className="image-content-wrapper FlexCenter">
+                      <img className="QuickLinkImage" id="project-link-image" src={ContactImage} alt="just a random image"></img>
+                      <div className="image-text-block">
+                        <div className="image-text-title">
+                          <h2 className="ImageTitle">Contact Us</h2>
+                        </div>
+                        <div className="image-text-description">
+                          <p className="ImageBody">Have questions, comments, or concerns? Want to know more ways that you can get involved? Contact us!</p>
+                        </div>
+                        <div className="button-wrapper FlexRowCenter">
+                          <div className="button-content">
+                            <a href="/" className="Button ImageButton FlexCenter">Contact</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  </div>
+                </div>
                 </div>
               </div>
             </section>
