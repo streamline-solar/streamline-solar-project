71a1b4a437b30c85baf0e96e9e4253faf59229a5
jesse-dahl <jddahl199@gmail.com>
2019-06-10 16:56:22 -0600

    added stuff the 'our progress' page

--- src/components/OurProgress.js
+++ src/components/OurProgress.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import "./../styles/index.scss";
 import "./../styles/progress.scss";
 import Logo from "./../images/streamline-solar-logo.png";
+import UniversitySupport from "./../images/progress-support.jpeg";
+import StudentInvolvement from "./../images/progress-student.jpeg";
 
 class Progress extends Component {
   render() {
@@ -65,6 +67,50 @@ class Progress extends Component {
 
                   <div className="seperator-block">
                     <hr className="seperator"></hr>
+                  </div>
+
+                  <div className="body-element-wrapper InlineBlock">
+                    <div className="body-element-content FlexColCenter">
+                      <div className="left-text-wrapper">
+                        <div className="left-text-content">
+                          <div className="left-text-title">
+                            <h1>University Support</h1>
+                          </div>
+                          <div className="left-text-body">
+                            <p className="SmallText">
+                              We have received support and are seeking counsel from a number of department heads and faculty members around campus. Montana State University is aware and excited about the Streamline Solar Stops project!
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="image-wrapper">
+                        <div className="image-image">
+                          <img className="progress-body-image" src={UniversitySupport} alt="just an image" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="body-element-wrapper InlineBlock">
+                    <div className="body-element-content FlexColCenter">
+                      <div className="left-text-wrapper">
+                        <div className="left-text-content">
+                          <div className="left-text-title">
+                            <h1>Student Involvement</h1>
+                          </div>
+                          <div className="left-text-body">
+                            <p className="SmallText">
+                             Thanks to extensive student outreach and cooperation between many of the departments on cmapus, we have been able to actively include several different Montana State Unviersity disciplines. This adds invaluable diversity which will increase the quality and outcome of the Streamline Solar Stops proejct.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="image-wrapper">
+                        <div className="image-image">
+                          <img className="progress-body-image" src={StudentInvolvement} alt="just an image" />
+                        </div>
+                      </div>
+                    </div>
                   </div>
 
                 </div>
