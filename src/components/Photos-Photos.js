8c1e1a641f7187ed44fe1de22c4a9a8207739f70
jesse-dahl <jddahl199@gmail.com>
2019-06-11 17:46:46 -0600

    added more stuff

---
+++ src/components/Photos-Photos.js
@@ -0,0 +1,15 @@
+import AlChE1 from "./../images/photos/AlChE/AlChE-0.jpeg";
+import AlChE2 from "./../images/photos/AlChE/AlChE-1.jpeg";
+import AlChE3 from "./../images/photos/AlChE/AlChE-2.jpeg";
+import AlChE4 from "./../images/photos/AlChE/AlChE-3.jpeg";
+import AlChE5 from "./../images/photos/AlChE/AlChE-4.jpeg";
+import AlChE6 from "./../images/photos/AlChE/AlChE-5.jpeg";
+import AlChE7 from "./../images/photos/AlChE/AlChE-6.jpeg";
+import AlChE8 from "./../images/photos/AlChE/AlChE-7.jpeg";
+
+import Bash1 from "./../images/photos/Bash/Bash-1.jpeg";
+import Bash2 from "./../images/photos/Bash/Bash-2.jpeg";
+import Bash3 from "./../images/photos/Bash/Bash-3.jpeg";
+import Bash4 from "./../images/photos/Bash/Bash-4.jpeg";
+import Bash5 from "./../images/photos/Bash/Bash-5.jpeg";
+import Bash6 from "./../images/photos/Bash/Bash-6.jpeg";
