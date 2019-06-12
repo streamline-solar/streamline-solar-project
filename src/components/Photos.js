b18f096c9e97cfc7f7ec6bace2eaf9dd462da3fd
jesse-dahl <jddahl199@gmail.com>
2019-06-11 21:19:17 -0600

    thank god for bootstrap...again. added carousels for the slideshows on the photos page

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Carousel from 'react-bootstrap/Carousel';
 import Logo from "./../images/streamline-solar-logo.png";
-import { pics } from "./Photos-Photos";
 import "./../styles/index.scss";
 import "./../styles/photos.scss";
+import "./../styles/bootstrap.scss";
+
+let handleSelect = (selectedIndex, e) => {
+  this.setState({
+    index: selectedIndex,
+    direction: e.direction,
+  });
+}
 
 class Photos extends Component {
 
@@ -67,9 +74,76 @@ class Photos extends Component {
                 </div>
                 <div className="main-slider-wrapper">
                   <div className="main-slider-content">
-                    <Carousel>
+                    <Carousel
+                    activeIndex={this.index} 
+                    direction={this.direction} 
+                    onSelect={this.handleSelect}
+                    pauseOnHover={false} >
                       <Carousel.Item>
-                        <img className="d-block w-100" src={pics.AlChE1} alt="pic" />
+                        <img className="d-block w-100" src={require("./../images/photos/AlChE/AlChE-0.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/AlChE/AlChE-1.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/AlChE/AlChE-2.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/AlChE/AlChE-3.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/AlChE/AlChE-4.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/AlChE/AlChE-5.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/AlChE/AlChE-6.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/AlChE/AlChE-7.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                    </Carousel>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="slideshow-row-wrapper">
+              <div className="slideshow-row-content">
+                <div className="slideshow-title-wrapper">
+                  <div className="slideshow-title">
+                    <h3>AlChE Climate Conference</h3>
+                  </div>
+                  <div className="photo-credz">
+                    <p>Photos by: Mike Allen</p>
+                    <p>Website by: Jesse Dahl</p>
+                  </div>
+                </div>
+                <div className="main-slider-wrapper">
+                  <div className="main-slider-content">
+                    <Carousel
+                    activeIndex={this.index} 
+                    direction={this.direction} 
+                    onSelect={this.handleSelect}
+                    pauseOnHover={false} >
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/Bash/Bash-1.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/Bash/Bash-2.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/Bash/Bash-3.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/Bash/Bash-4.jpeg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/Bash/Bash-5.jpg")} alt="pic" />
+                      </Carousel.Item>
+                      <Carousel.Item>
+                        <img className="d-block w-100" src={require("./../images/photos/Bash/Bash-6.jpeg")} alt="pic" />
                       </Carousel.Item>
                     </Carousel>
                   </div>
